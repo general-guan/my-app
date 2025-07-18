bf17ab84cd2c3bb1d99e58583b8995c742fd54d6
guanjunjiang <934750771@qq.com>
2025-07-18 11:08:31 +0800

    引入无头UI、图标、头部

---
+++ src/layout/header/index.tsx
@@ -0,0 +1,8 @@
+import ThemeSwitcher from "./theme-switcher";
+export default function Header() {
+  return (
+    <div>
+      <ThemeSwitcher />
+    </div>
+  );
+}
---
+++ src/layout/header/theme-switcher.tsx
@@ -0,0 +1,3 @@
+export default function ThemeSwitcher() {
+  return <div>1</div>;
+}
