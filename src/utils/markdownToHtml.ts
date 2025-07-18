7038e837fc3518f68e71e0cce68a9f568836b726
guanjunjiang <934750771@qq.com>
2025-07-18 10:04:30 +0800

    新增暗黑模式

--- src/utils/markdownToHtml.ts
+++ src/utils/markdownToHtml.ts
@@ -1,12 +1,19 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
+import rehypeShiki from "@shikijs/rehype";
 import rehypeStringify from "rehype-stringify";
 
 export async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse) // 解析 Markdown
     .use(remarkRehype) // 转换 Markdown AST 为 HTML AST
+    .use(rehypeShiki, {
+      themes: {
+        light: "vitesse-light",
+        dark: "vitesse-dark",
+      },
+    })
     .use(rehypeStringify) // 序列化 HTML AST 为字符串
     .process(markdown);
   return String(result);
