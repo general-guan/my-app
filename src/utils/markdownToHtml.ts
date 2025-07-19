0c3465ef22aa28023449bc54e05ef2f4aae70c1d
guanjunjiang <934750771@qq.com>
2025-07-19 11:05:29 +0800

    添加ID

--- src/utils/markdownToHtml.ts
+++ src/utils/markdownToHtml.ts
@@ -3,11 +3,15 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeShiki from "@shikijs/rehype";
 import rehypeStringify from "rehype-stringify";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse) // 解析 Markdown
     .use(remarkRehype) // 转换 Markdown AST 为 HTML AST
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings)
     .use(rehypeShiki, {
       themes: {
         light: "vitesse-light",
